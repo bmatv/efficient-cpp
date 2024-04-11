16439de8b8c223c48aaa1027b77e4108986a7f35
Bogdan Matviichuk <32863407+bmatv@users.noreply.github.com>
2024-04-11 16:52:59 +1000

    standalone reinterpret_cast test function added

---
+++ reinterp.cpp
@@ -0,0 +1,12 @@
+//
+// Created by bogdan on 11/04/24.
+//
+int main(){
+    float arr[]{0.1,0.2,0.3,0.4,0.5};
+//    int* a;
+    for(int i = 0; i<5;++i){
+        auto a = reinterpret_cast<int*>(arr+i);
+
+        }
+
+}
