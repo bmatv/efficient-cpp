c0c2fe8d1a8224f3dcde134c91f2883ab04b0f25
Bogdan Matviichuk <32863407+bmatv@users.noreply.github.com>
2024-06-28 13:27:17 +1000

    fast pythagorean test added

---
+++ fast_pythagorean.C
@@ -0,0 +1,182 @@
+#include "benchmark/benchmark.h"
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+
+void BM_pythagorean(benchmark::State& state){
+    srand(1);
+    unsigned int N = state.range(0);
+    std::vector<int> v1(N), v2(N);
+    std::vector<int>c1(N);
+
+    for(size_t i=0;i<N;++i){
+        v1[i] = rand();
+        v2[i] = rand();
+    }
+    int* p1 = v1.data(), *p2 = v2.data();
+    int* b1 = c1.data();
+
+    for (auto _:state){
+        for (size_t i =0;i<N;++i){
+            b1[i] = sqrt(p1[i]*p1[i] + p2[i]*p2[i]);
+        }
+    benchmark::DoNotOptimize(c1);
+    benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(N*state.iterations());
+    // std::cout << "1 " << c1[0] << ' ' << v1[0] <<' ' <<v2[0] << '\n';
+}
+
+
+void BM_pythagorean_pow(benchmark::State& state){
+    srand(1);
+    unsigned int N = state.range(0);
+    std::vector<int> v1(N), v2(N);
+    std::vector<int>c1(N);
+
+    for(size_t i=0;i<N;++i){
+        v1[i] = rand();
+        v2[i] = rand();
+    }
+    // int* p1 = v1.data(), *p2 = v2.data();
+    // int* b1 = c1.data();
+
+    for (auto _:state){
+        for (size_t i =0;i<N;++i){
+            c1[i] = pow(pow(v1[i],2) + pow(v2[i],2),0.5);
+        }
+    benchmark::DoNotOptimize(c1);
+    benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(N*state.iterations());
+    // std::cout << "2 " << c1[0] << ' ' << v1[0] <<' ' <<v2[0] << '\n';
+}
+
+
+void BM_fast_pythagorean(benchmark::State& state){
+    // https://en.wikipedia.org/wiki/Alpha_max_plus_beta_min_algorithm
+    srand(1);
+    unsigned int N = state.range(0);
+    std::vector<int> v1(N), v2(N);
+    std::vector<int>c1(N);
+    
+    auto alpha0 = 2*cos(M_PI/8) / (1+cos(M_PI/8));
+    auto beta0  = 2*sin(M_PI/8) / (1+cos(M_PI/8));
+
+    for(size_t i=0;i<N;++i){
+        v1[i] = rand();
+        v2[i] = rand();
+    }
+    int* p1 = v1.data(), *p2 = v2.data();
+    int* b1 = c1.data();
+
+    for (auto _:state){
+        for (size_t i =0;i<N;++i){
+            auto& vmax = std::max(p1[i],p2[i]); // marginally faster than pass by copy
+            auto& vmin = std::min(p1[i],p2[i]);
+            b1[i] = alpha0*vmax + beta0*vmin;
+        }
+    benchmark::DoNotOptimize(c1);
+    benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(N*state.iterations());
+    // std::cout << "3 " << c1[0] << ' ' << v1[0] <<' ' <<v2[0] << '\n';
+}
+
+void BM_fast_pythagorean_2(benchmark::State& state){
+    // https://en.wikipedia.org/wiki/Alpha_max_plus_beta_min_algorithm
+    srand(1);
+    unsigned int N = state.range(0);
+    std::vector<int> v1(N), v2(N);
+    std::vector<int> c1(N,0);
+    
+    auto alpha0 = 2*cos(M_PI/8) / (1+cos(M_PI/8));
+    auto beta0  = 2*sin(M_PI/8) / (1+cos(M_PI/8));
+
+    for(size_t i=0;i<N;++i){
+        v1[i] = rand();
+        v2[i] = rand();
+    }
+
+    for (auto _:state){
+        for (size_t i =0;i<N;++i){
+            auto out = std::minmax(v1[i],v2[i]);
+            c1[i] = alpha0*out.second + beta0*out.first;
+        }
+    benchmark::DoNotOptimize(c1);
+    benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(N*state.iterations());
+    // std::cout << "4 " << c1[0] << ' ' << v1[0] <<' ' <<v2[0] << '\n';
+}
+
+void BM_fast_pythagorean_3(benchmark::State& state){
+    // https://en.wikipedia.org/wiki/Alpha_max_plus_beta_min_algorithm
+    srand(1);
+    unsigned int N = state.range(0);
+    std::vector<int> v1(N), v2(N);
+    std::vector<int> c1(N);
+    
+    auto alpha0 = 2*cos(M_PI/8) / (1+cos(M_PI/8));
+    auto beta0  = 2*sin(M_PI/8) / (1+cos(M_PI/8));
+
+    for(size_t i=0;i<N;++i){
+        v1[i] = rand();
+        v2[i] = rand();
+    }
+    int* p1 = v1.data(), *p2 = v2.data();
+    int* b1 = c1.data();
+
+    for (auto _:state){
+        for (size_t i =0;i<N;++i){
+            auto[vmax,vmin] = std::minmax(p1[i],p2[i]);
+            b1[i] = alpha0*vmax + beta0*vmin;
+        }
+    benchmark::DoNotOptimize(c1);
+    benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(N*state.iterations());
+    // std::cout << "5 " << c1[0] << ' ' << v1[0] <<' ' <<v2[0] << '\n';
+}
+
+void BM_fast_pythagorean_4(benchmark::State& state){
+    // https://en.wikipedia.org/wiki/Alpha_max_plus_beta_min_algorithm
+    srand(1);
+    unsigned int N = state.range(0);
+    std::vector<int> v1(N), v2(N);
+    std::vector<int> c1(N);
+    
+    auto alpha0 = 2*cos(M_PI/8) / (1+cos(M_PI/8));
+    auto beta0  = 2*sin(M_PI/8) / (1+cos(M_PI/8));
+
+    for(size_t i=0;i<N;++i){
+        v1[i] = rand();
+        v2[i] = rand();
+    }
+    int* p1 = v1.data(), *p2 = v2.data();
+
+    for (auto _:state){
+        for (size_t i =0;i<N;++i){
+            // auto[vmax,vmin] = std::minmax(p1[i],p2[i]);
+            double s [2] = {alpha0*p1[i] + beta0*p2[i], alpha0*p2[i]+beta0*p1[i]};
+            
+            c1[i] = s[p2[i]>p1[i]];
+        }
+    benchmark::DoNotOptimize(c1);
+    benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(N*state.iterations());
+    std::cout << "6 " << c1[0] << ' ' << v1[0] <<' ' <<v2[0] << '\n';
+}
+
+BENCHMARK(BM_pythagorean)->Arg(1<<22);
+BENCHMARK(BM_pythagorean_pow)->Arg(1<<22);
+
+BENCHMARK(BM_fast_pythagorean)->Arg(1<<22);
+BENCHMARK(BM_fast_pythagorean_2)->Arg(1<<22);
+BENCHMARK(BM_fast_pythagorean_3)->Arg(1<<22);
+BENCHMARK(BM_fast_pythagorean_4)->Arg(1<<22);
+
+
+BENCHMARK_MAIN();
