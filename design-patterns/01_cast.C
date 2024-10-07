d1de53080a87d4a341ec41b5bad7678fe646f256
Bogdan Matviichuk <32863407+bmatv@users.noreply.github.com>
2024-10-07 21:18:24 +1100

    init file reformatting

--- design-patterns/01_cast.C
+++ design-patterns/01_cast.C
@@ -1,38 +1,41 @@
-#include <stdlib.h>
-#include <stdint.h>
+#include <cstdlib>
+
 #include <iostream>
 
 class Base1 {
-    protected:
-    int i = 0;
-    public:
-    virtual ~Base1() {}
-    virtual int f() { return ++i; }
+protected:
+  int i = 0;
+
+public:
+  virtual ~Base1() {}
+  virtual int f() { return ++i; }
 };
 
 class Base2 {
-    protected:
-    int j = 0;
-    public:
-    virtual ~Base2() {}
-    virtual int g() { return ++j; }
+protected:
+  int j = 0;
+
+public:
+  virtual ~Base2() {}
+  virtual int g() { return ++j; }
 };
 
-class Derived : public Base1, public Base2 {
-};
+class Derived : public Base1, public Base2 {};
 
 int main() {
-    {
-        Derived d;
-        Derived* p = &d;
-        std::cout << "Derived: " << (void*)(p) <<
-                     " Base1: " << (void*)(static_cast<Base1*>(p)) <<
-                     " Base2: " << (void*)(static_cast<Base2*>(p)) << std::endl;
-    }
-    {
-        Derived* p = nullptr;
-        std::cout << "Derived: " << (void*)(p) <<
-                     " Base1: " << (void*)(static_cast<Base1*>(p)) <<
-                     " Base2: " << (void*)(static_cast<Base2*>(p)) << std::endl;
-    }
+  {
+    Derived d;
+
+    // Derived* p = &d;
+    Derived *p = nullptr;
+    std::cout << "Derived: " << (void *)(p)
+              << " Base1: " << (void *)(static_cast<Base1 *>(p))
+              << " Base2: " << (void *)(static_cast<Base2 *>(p)) << std::endl;
+  }
+  {
+    Derived *p = nullptr;
+    std::cout << "Derived: " << (void *)(p)
+              << " Base1: " << (void *)(static_cast<Base1 *>(p))
+              << " Base2: " << (void *)(static_cast<Base2 *>(p)) << std::endl;
+  }
 }
